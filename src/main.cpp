ef6abf652fa149e6dba6b7d175c05e8ab1ad79da
Kota <Askt@Akst.local>
2019-12-22 15:38:50 +0900

    add arg parser

--- src/main.cpp
+++ src/main.cpp
@@ -17,13 +17,30 @@ int main(int argc, char** argv)
      * Parse arguments
      */
     tskd::util::Option option;
-    option.set_input_path("../benchmarks/partest.qc");
+
+    const std::string input = argv[1];
+    const std::string path = "../benchmarks/" + input;
+
+    const std::string dec_type = argv[2];
+    if (dec_type == "gauss")
+    {
+        option.set_dec_type(DecompositionType::kgauss);
+    }
+    else if (dec_type == "par")
+    {
+        option.set_dec_type(DecompositionType::kparallel);
+    }
+    else
+    {
+        option.set_dec_type(DecompositionType::kgauss);
+    }
+
+    option.set_input_path(path);
     option.set_num_distillation(4);
     option.set_distillation_step(20);
     option.set_change_row_order(false);
     option.set_syn_method((SynthesisMethod::ktpar));
     option.set_part_type(PartitionType::kmatroid);
-    option.set_dec_type(DecompositionType::kgauss);
     option.show();
 
     std::cout << "-->> read file" << std::endl;
